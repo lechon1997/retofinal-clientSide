35128e7a9828aef55728ed01cc005f939d53d093
Eddi <eddy-bermejo@hotmail.com>
2022-03-30 08:47:22 -0500

    Reto final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Logo from './assets/img/logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div-welcome">
+      <img src={Logo} alt="" />
+      <h1>Reto final - Client Side</h1>
     </div>
   );
 }
