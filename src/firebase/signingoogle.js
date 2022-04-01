5e81b473eae3ebc6f46538f1619a2229a42f7678
lechon1997 <odriozola-leo@hotmail.com>
2022-03-31 23:00:39 -0300

    autenticación con google

---
+++ src/firebase/signingoogle.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDireMgP0XQM-QFya41RrzWrOnD_6npTNw",
+  authDomain: "sofka-facturacion-dfe34.firebaseapp.com",
+  projectId: "sofka-facturacion-dfe34",
+  storageBucket: "sofka-facturacion-dfe34.appspot.com",
+  messagingSenderId: "963976525403",
+  appId: "1:963976525403:web:e86585216473bd2e77317e",
+};
+
+// Initialize Firebase
+const signingoogle = initializeApp(firebaseConfig);
+export default signingoogle;
